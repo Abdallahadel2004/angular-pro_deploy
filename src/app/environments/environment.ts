af2f885d8647153b640f9ab475cd82c6c8db4fd9
mahmoudrabbas <mahmoudrabass11@gmail.com>
2026-03-12 02:40:50 +0200

    mahmoud-last

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,5 +1,9 @@
 export const environment = {
   production: false,
+<<<<<<< Updated upstream
   //apiUrl: 'https://back-omega-amber.vercel.app',
+=======
+  // apiUrl: 'https://back-omega-amber.vercel.app',
+>>>>>>> Stashed changes
   apiUrl: 'http://localhost:3000',
 };
